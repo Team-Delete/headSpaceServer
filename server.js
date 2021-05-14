f657a43ca395513c70353d5442d2d7a6217366ce
Anthony Joel Williams <williamsjanthony15@yahoo.com>
2021-05-13 20:52:31 -0500

    initial server push

---
+++ server.js
@@ -0,0 +1,15 @@
+'use strict';
+
+// onst mongoose = require('mongoose');
+// const express = require('express');
+// const app = express();
+
+// require('dotenv').config();
+// app.use(express.json());
+
+// const PORT = process.env.PORT || 3001;
+
+// const cors = require('cors');
+// app.use (cors());
+
+// app.listen(PORT, () => console.log(`Listening on ${PORT}`));
