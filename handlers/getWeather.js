a8b738b5af5197410e42688b831de38351905e93
Adrian Milsap <adrian@hirobius.com>
2021-05-17 21:28:41 -0700

    handlers in modules

--- handlers/getWeather.js
+++ handlers/getWeather.js
@@ -4,7 +4,7 @@ function getWeather(request, response) {
   const weatherQuery = request.query.weatherSearch;
   const url = 'https://api.weatherbit.io/v2.0/current';
   const query = {
-    cityName: request.query.cityName,
+    // cityName: request.query.cityName,
     key: process.env.WEATHERBIT_API_KEY,
     lat: request.query.lat,
     lon: request.query.lon,
@@ -18,11 +18,18 @@ function getWeather(request, response) {
     });
 };
 
+// https://medium.com/@DylanAttal/convert-celsius-to-fahrenheit-in-javascript-b6b76b47c4f0#:~:text=Luckily%20for%20us%2C%20the%20good,9%2F5%20then%20add%2032.
+
+function convertToF(celsius) {
+  let fahrenheit = celsius * 9/5 + 32
+  return fahrenheit;
+}
+
 function Forecast(day) {
   this.date = day.datetime;
   this.description = day.weather.description;
-  this.clouds = day.clouds;
-  this.temp = day.temp;
+  this.clouds = `${day.clouds} clouds`;
+  this.temp = `${convertToF(day.temp)}°F`;
   this.directionString = day.wind_cdir_full;
   this.directionDegree = day.wind_dir
 }
